a0e77de006b5d13ab03d495e1ecda893ec4a3472
Tran Duc Tam ADO) <tamtd.vitalify.asia@mjs.co.jp>
2023-04-02 17:18:20 +0700

    get data from be

---
+++ app/constant/constant.ts
@@ -0,0 +1,9 @@
+const variable = {
+    USER_DATA: "userData",
+    EMPTY_OBJECT: {},
+    CURRENT_SONG_DATA: "currentSongData",
+    LOADING_URL: "https://media.giphy.com/media/VseXvvxwowwCc/giphy.gif",
+    BACKEND_URL: "http://127.0.0.1:8000/api",
+};
+
+export { variable };
