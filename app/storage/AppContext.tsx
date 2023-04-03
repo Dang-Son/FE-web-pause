812a57dc6079a82d60a4cd3043e513259bf0e31a
Dang-Son <19522131@gm.uit.edu.vn>
2023-04-03 16:33:03 +0700

    load-home-screen

--- app/storage/AppContext.tsx
+++ app/storage/AppContext.tsx
@@ -2,52 +2,62 @@ import { createContext } from "react";
 import type { MyState } from "./AppState";
 
 type TAppContext = MyState & {
-    toggleIsPlaying: () => void;
-    updateState: (key: any, value: any) => void;
+  toggleIsPlaying: () => void;
+  updateState: (key: any, value: any) => void;
 };
 export default createContext<TAppContext>({
-    currentSongData: {
-        album: "Swimming",
-        artist: "Mac Miller",
-        image: "swimming",
-        length: 312,
-        id: 1,
-        title: "So It Goes",
-        isPlaying: false,
-        audioURL:
-            "https://res.cloudinary.com/dck0bidwh/video/upload/v1675850974/bs1sjno6gknfldz4iwub.mp3",
-    },
-    isLoading: true,
-    showMusicBar: true,
-    userData: "",
-    sound: undefined,
-    toggleIsPlaying: () => {},
-    updateState: () => {},
+  currentSongData: {
+    album: "Swimming",
+    artist: "Mac Miller",
+    image: "swimming",
+    length: 312,
+    id: 1,
+    title: "So It Goes",
+    isPlaying: false,
+    audioURL:
+      "https://res.cloudinary.com/dck0bidwh/video/upload/v1675850974/bs1sjno6gknfldz4iwub.mp3",
+  },
+  isLoading: true,
+  showMusicBar: true,
+  userData: "",
+  sound: undefined,
+  toggleIsPlaying: () => {},
+  updateState: () => {},
 });
 
 export type TSong = {
-    attributes: {
-        audioURL: string;
-        author_name: string;
-        imageURL: string;
-        name: string;
-        liked: number;
-        playlist_id: string;
-        views: number;
-        id: string;
-    };
+  attributes: {
+    audioURL: string;
+    author_name: string;
+    imageURL: string;
+    name: string;
+    liked: number;
+    playlist_id: string;
+    views: number;
+    id: string;
+  };
 
-    relationships: {
-        artist: {
-            data: TArtist;
-        };
+  relationships: {
+    artist: {
+      data: TArtist;
     };
+  };
 };
 
 export type TArtist = {
-    attributes: {
-        name: string;
-        user_id: string;
-        followed: number;
-    };
+  attributes: {
+    name: string;
+    user_id: string;
+    followed: number;
+  };
+};
+
+export type TPlaylist = {
+  attributes: {
+    id: number;
+    category: string;
+    name: string;
+    user_created_name: string;
+    imageURL: string;
+  };
 };
